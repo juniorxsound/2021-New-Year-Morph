a1ed77c56c331090ea178d646a1883dbd96136a1
juniorxsound <or.fleisher@nytimes.com>
2021-12-23 16:35:02 -0500

    Try adding a base to vite

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
 export default defineConfig({
+    base: '/2021-New-Year-Morph/',
     plugins: [reactRefresh()],
 })
